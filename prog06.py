8ed1c7192a92955613f11ed6d676819345c6dd72
cnriveraa <rivera.cn18@gmail.com>
2025-03-07 17:32:10 +0800

    Update prog06.py

--- prog06.py
+++ prog06.py
@@ -1,1 +1,6 @@
 #Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+numbers = [int(input(f"Enter number {i + 1}: ")) for i in range(10)]
+
+first_number = numbers[0]
+for num in numbers[1:]:
+    print(f"{first_number} - {num} = {first_number - num}")
