3be3727ff3755cd9bdbaa8012bb412e6649fdb8b
cnriveraa <rivera.cn18@gmail.com>
2025-03-07 17:45:19 +0800

    Create prog10.py

---
+++ prog10.py
@@ -0,0 +1,5 @@
+#Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+for i in range(num1 + 1, num2):
+    print(i)
